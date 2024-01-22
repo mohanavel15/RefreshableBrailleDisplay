960914d28b50b0004bbebe8be10003bcec5a2819
Mohan <mohanavel15@protonmail.com>
2024-01-22 22:06:57 +0530

    app rework: Done. Review code once before merging

--- api_server/server.py
+++ api_server/server.py
@@ -36,17 +36,8 @@ mqtt_client = Mqtt(app)
 def handle_connect(client, userdata, flags, rc):
     if rc == 0:
         print('Connected successfully')
-        mqtt_client.subscribe(app.config['MQTT_TOPIC'])
     else:
         print('Bad connection. Code:', rc)
-
-@mqtt_client.on_message()
-def handle_mqtt_message(client, userdata, message):
-    data = dict(
-        topic=message.topic,
-        payload=message.payload.decode()
-    )
-    print('Received message on topic: {topic} with payload: {payload}'.format(**data))
 
 @app.route('/pdf', methods=['POST'])
 def upload_pdf():
